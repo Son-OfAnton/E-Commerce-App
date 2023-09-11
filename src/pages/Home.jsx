97c033241abcf8855a5e1b8372da36eced821545
Son-OfAnton <admasterefe00@gmail.com>
2023-09-11 16:41:57 +0300

    feat: Add slider and categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,93 +1,15 @@
-import { styled } from "styled-components";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
 import NavBar from "../components/NavBar";
-import { Search, ShoppingCartOutlined } from "@material-ui/icons";
-import { Badge } from "@material-ui/core";
-
-const Container = styled.div`
-  height: 60px;
-`
-
-const Wrapper = styled.div`
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-`
-
-const Language = styled.span`
-  font-size: 14px;
-  cursor: pointer;
-`
-
-const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-  padding: 5px;
-`
-
-const Input = styled.input`
-  border: none;
-`
-
-const Center = styled.div`
-  flex: 1;
-  text-align: center;
-`
-
-const Logo = styled.h1`
-  font-weight: bold;
-`
-
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`
-
-const MenuItem = styled.div`
-  font-size: 14px;
-  cursor: pointer;
-  margin-left: 25px;
-`
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      <Container>
-        <Wrapper>
-          <Left>
-            <Language>EN</Language>
-            <SearchContainer>
-              <Input />
-              <Search style={{color: "gray", fontSize: 16}}/>
-            </SearchContainer>
-          </Left>
-
-          <Center>
-            <Logo>SHOP</Logo>
-          </Center>
-
-          <Right>
-            <MenuItem>REGISTER</MenuItem>
-            <MenuItem>SIGN IN</MenuItem>
-            <MenuItem>
-              <Badge badgeContent={4} color="primary">
-                <ShoppingCartOutlined />
-              </Badge>
-            </MenuItem>
-          </Right>
-          {/* <NavBar /> */}
-        </Wrapper>
-      </Container>
+      <Announcement />
+      <NavBar />
+      <Slider />
+      <Categories />
     </div>
   );
 };
